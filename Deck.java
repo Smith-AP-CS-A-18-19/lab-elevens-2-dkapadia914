eed38fdca2d8469f0f229eb5bbf90b02bd0f5a65
Dev Kapadia <kapaddev@berkeleyprep.org>
2018-11-16 08:54:24 -0500

    thanks g

--- Deck.java
+++ Deck.java
@@ -8,118 +8,134 @@ import java.util.ArrayList;
  */
 public class Deck {
 
-	/**
-	 * cards contains all the cards in the deck.
-	 */
-	private Card[] cards;
+        /**
+         * cards contains all the cards in the deck.
+         */
+        private Card[] cards;
 
-	/**
-	 * size is the number of not-yet-dealt cards.
-	 * Cards are dealt from the top (highest index) down.
-	 * The next card to be dealt is at size - 1.
-	 */
-	private int size;
-
-
-	/**
-	 * Creates a new <code>Deck</code> instance.<BR>
-	 * It pairs each element of ranks with each element of suits,
-	 * and produces one of the corresponding card.
-	 * @param ranks is an array containing all of the card ranks.
-	 * @param suits is an array containing all of the card suits.
-	 * @param values is an array containing all of the card point values.
-	 */
-	public Deck(String[] ranks, String[] suits, int[] values) {
-		cards = new Card[ranks.length * suits.length];
-		int rankIndex = 0;
-		int suitIndex = 0;
-		for (int i = 0; i < cards.length; i++) {
-			Card card = new Card(ranks[rankIndex], suits[suitIndex], values[rankIndex]);
-			cards[i] = card;
-			rankIndex = (rankIndex + 1) % ranks.length;
-			if (rankIndex == 0) {
-				suitIndex++;
-			}
-		}
-	}
+        /**
+         * size is the number of not-yet-dealt cards.
+         * Cards are dealt from the top (highest index) down.
+         * The next card to be dealt is at size - 1.
+         */
+        private int size;
 
 
-	/**
-	 * Determines if this deck is empty (no undealt cards).
-	 * @return true if this deck is empty, false otherwise.
-	 */
-	public boolean isEmpty() {
-		if (size == 0) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
 
-	/**
-	 * Accesses the number of undealt cards in this deck.
-	 * @return the number of undealt cards in this deck.
-	 */
-	public int size() {
-		return size;
-	}
+        /**
+         * Creates a new <code>Deck</code> instance.<BR>
+         * It pairs each element of ranks with each element of suits,
+         * and produces one of the corresponding card.
+         * @param ranks is an array containing all of the card ranks.
+         * @param suits is an array containing all of the card suits.
+         * @param values is an array containing all of the card point values.
+         */
+        public Deck(String[] ranks, String[] suits, int[] values) {
+                cards = new Card[ranks.length * suits.length];
+                size = ranks.length * suits.length;
+                int rankIndex = 0;
+                int suitIndex = 0;
+                for (int i = 0; i < cards.length; i++) {
+                        Card card = new Card(ranks[rankIndex], suits[suitIndex], values[rankIndex]);
+                        cards[i] = card;
+                        rankIndex = (rankIndex + 1) % ranks.length;
+                        if (rankIndex == 0) {
+                                suitIndex++;
+                        }
+                }
 
-	/**
-	 * Randomly permute the given collection of cards
-	 * and reset the size to represent the entire deck.
-	 */
-	public void shuffle() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 4 *** */
-	}
+        }
 
-	/**
-	 * Deals a card from this deck.
-	 * @return the card just dealt, or null if all the cards have been
-	 *         previously dealt.
-	 */
-	public Card deal() {
-		if (size > 0) {
-			size = size - 1;
-			Card cardReturned = cards[size];
-			return cardReturned;
-		} else {
-			return null;
-		}
-	}
 
-	/**
-	 * Generates and returns a string representation of this deck.
-	 * @return a string representation of this deck.
-	 */
-	@Override
-	public String toString() {
-		String rtn = "size = " + size + "\nUndealt cards: \n";
+        /**
+         * Determines if this deck is empty (no undealt cards).
+         * @return true if this deck is empty, false otherwise.
+         */
+        public boolean isEmpty() {
 
-		for (int k = size - 1; k >= 0; k--) {
-			rtn = rtn + cards.get(k);
-			if (k != 0) {
-				rtn = rtn + ", ";
-			}
-			if ((size - k) % 2 == 0) {
-				// Insert carriage returns so entire deck is visible on console.
-				rtn = rtn + "\n";
-			}
-		}
+                if (size == 0) {
+                        return true;
+                } else {
+                        return false;
+                }
+        }
 
-		rtn = rtn + "\nDealt cards: \n";
-		for (int k = cards.size() - 1; k >= size; k--) {
-			rtn = rtn + cards.get(k);
-			if (k != size) {
-				rtn = rtn + ", ";
-			}
-			if ((k - cards.size()) % 2 == 0) {
-				// Insert carriage returns so entire deck is visible on console.
-				rtn = rtn + "\n";
-			}
-		}
+        /**
+         * Accesses the number of undealt cards in this deck.
+         * @return the number of undealt cards in this deck.
+         */
+        public int size() {
 
-		rtn = rtn + "\n";
-		return rtn;
-	}
+                return size;
+        }
+
+        /**
+         * Randomly permute the given collection of cards
+         * and reset the size to represent the entire deck.
+         */
+        public void shuffle() {
+                // for (int i = cards.size() - 1; i > 0; i--) {
+                //       int o = i + 1;
+                //       int temporary = 0;
+                //       int randomInt = (int)(Math.random() * o) + temporary;
+
+                // IGNORE THIS FOR NOW
+
+
+                // RANDOMIZE CARDS AND RESET THE SIZE
+                // }
+
+                // size = cards.length - 1;
+
+
+        }
+
+        /**
+         * Deals a card from this deck.
+         * @return the card just dealt, or null if all the cards have been
+         *         previously dealt.
+         */
+        public Card deal() {
+                if (size > 0) {
+                        size--;
+                        return cards[size];
+                } else {
+                        return null;
+                }
+        }
+
+        /**
+         * Generates and returns a string representation of this deck.
+         * @return a string representation of this deck.
+         */
+        @Override
+        public String toString() {
+                String rtn = "size = " + size + "\nUndealt cards: \n";
+
+                for (int k = size - 1; k >= 0; k--) {
+                        rtn = rtn + cards[k];
+                        if (k != 0) {
+                                rtn = rtn + ", ";
+                        }
+                        if ((size - k) % 2 == 0) {
+                                // Insert carriage returns so entire deck is visible on console.
+                                rtn = rtn + "\n";
+                        }
+                }
+
+                rtn = rtn + "\nDealt cards: \n";
+                for (int k = (cards.length - 1); k >= size; k--) {
+                        rtn = rtn + cards[k];
+                        if (k != size) {
+                                rtn = rtn + ", ";
+                        }
+                        if ((k - cards.length) % 2 == 0) {
+                                // Insert carriage returns so entire deck is visible on console.
+                                rtn = rtn + "\n";
+                        }
+                }
+
+                rtn = rtn + "\n";
+                return rtn;
+        }
 }
--- DeckTester.java
+++ DeckTester.java
@@ -10,15 +10,63 @@ public class DeckTester {
 
         public static void main(String[] args) {
                 /* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
-                String ranks2[] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jacks", "Queens", "Kings"};
+
+                String ranks2[] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "Jacks", "Queens", "Kings"};
                 String suits2[] = {"Spades" ,"Clubs", "Hearts", "Diamonds"};
                 int pointValues2[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-								Deck e = new Deck(ranks2[], suits2[], pointValues2[]);
-								for (int i = 0; i < ranks2.length * suits2.length; i++) {
-									System.out.println(e.deal());
-								}
-								System.out.println(e.isEmpty())
 
+                Deck e = new Deck(ranks2, suits2, pointValues2);
+
+                for (int i = 0; i < (ranks2.length * suits2.length); i++) {
+                        System.out.println(e.deal());
+                }
+
+                System.out.println(e.isEmpty());
+
+
+
+
+
+//                       for (int i = 0; i < 52; i++) {
+//                               String rank = "";
+//                               String suit = "";
+//                       if ( (i+1) % 13 == 11 ) {
+//                               rank = "Jack";
+//
+//                       } else if ( (i+1) % 13 == 12 ) {
+//                               rank = "Queen";
+//
+//                       } else if ( (i+1) % 13 == 0 ) {
+//                               rank = "King";
+//
+//                       } else if ( (i+1) % 13 == 1 ) {
+//                               rank = "Ace";
+//                       } else {
+//
+//                               rank = Integer.toString((i + 1) % 13);
+//                       }
+//
+//                       if (i < 13) {
+//                               suit = "Hearts";
+//
+//                       } else if (i < 26) {
+//                               suit = "Diamonds";
+//
+//                       }  else if (i < 39) {
+//                               suit = "Clubs";
+//
+//                       } else {
+//                               suit = "Spades";
+//                       }
+//
+//                       ranks2[i] = rank;
+//                       suits2[i] = suit;
+//                       pointValues2[i] = ((i+1) % 13);
+// }
+//
+//
+//
+//
 
 
         }
